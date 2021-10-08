752609d3096bc6add1b91104b0ef511ff5a788fd
zuzuc <zuzu@posteo.de>
2021-10-08 18:10:22 +0200

    update project cards

--- src/data/projectData.jsx
+++ src/data/projectData.jsx
@@ -15,9 +15,32 @@ const projectData = [
             'Platform dedicated to data-visualisation through maps and sharing real-life stories'
     },
     {
-       
+        title: 'MapStories',
+        image: mapstoriesImg,
+        alt: 'Image of Map Stories',
+        url: 'https://map-stories.netlify.app/',
+        techStacks: [
+            'React',
+            'JavaScript',
+            'Node JS',
+        ],
         description:
-            ''
+            'Platform dedicated to data-visualisation through maps and sharing real-life stories'
+       
+    }, 
+    {
+        title: 'MapStories',
+        image: mapstoriesImg,
+        alt: 'Image of Map Stories',
+        url: 'https://map-stories.netlify.app/',
+        techStacks: [
+            'React',
+            'JavaScript',
+            'Node JS',
+        ],
+        description:
+            'Platform dedicated to data-visualisation through maps and sharing real-life stories'
+       
     }, 
 
 ]
