8d939798b83fe3b901eba3e808d68f7d39b315a1
zuzuc <zuzu@posteo.de>
2021-10-08 18:08:22 +0200

    Update project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,32 +6,37 @@ import projectData from "./../data/projectData";
 function Projects(){
     return (
         <>
-        <div className="section-font">Projects</div>
-            <div className="subheading-font">Visit my{" "}
-                <a
-                    href="https://github.com/zuzuc"
-                    className="link"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >Github</a>{" "}
-                    page to view current projects. 
-            </div>
-            <div className="grids-container">
-                <div className="grids-wrapper">
-                    {projectData.map((project) => (
-                        <Card
-                            key={project.title}
-                            title={project.title}
-                            image={project.image}
-                            techStacks={project.techStacks}
-                            description={project.description}
-                            alt={`Screenshot of ${project.title}`}
-                            url={project.url}
-                        />
-                ))}
+          <div className="section-container">
+              <div className="headline-container">
+                <div className="section-font">Projects</div>
+                  <div className="subheading-font">Visit my{" "}
+                    <a
+                      href="https://github.com/zuzuc"
+                      className="link"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      >Github</a>{" "}
+                      page to view current projects. 
+                  </div>  
                 </div>
-            </div>
+                <div className="grids-container">
+                    <div className="grids-wrapper">
+                        {projectData.map((project) => (
+                            <Card
+                                key={project.title}
+                                title={project.title}
+                                image={project.image}
+                                techStacks={project.techStacks}
+                                description={project.description}
+                                alt={`Screenshot of ${project.title}`}
+                                url={project.url}
+                            />
+                    ))}
+                    </div>
+                </div>
+          </div>
         </>
+
     );
 };
 
