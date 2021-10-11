3e3de870885a5acfce61a337d15c7bb11ce57aea
zuzuc <zuzu@posteo.de>
2021-10-12 00:55:08 +0200

    updates

--- src/components/TypewriterCycle.jsx
+++ src/components/TypewriterCycle.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
 import TypeWriter from "./TypeWriter";
+import PropTypes from "prop-types";
+
 
 function TypewriterCycle({ items }) {
   const [currentIndex, setCurrentIndex] = useState(0);
