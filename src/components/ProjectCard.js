af425eb48040584cd309409e146fda939286ecc0
zuzuc <zuzu@posteo.de>
2025-01-29 18:38:35 +0100

    Add button to project image

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import laptopImg from "./../assets/images/laptop.svg";
 
 function ProjectCard({ title, tags, description, image, url }) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="project-card">
+    <div
+      className="project-card"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       {/* Left Section - Project Details */}
       <div className="project-content">
         <h3 className="project-title">
@@ -20,23 +26,29 @@ function ProjectCard({ title, tags, description, image, url }) {
         </div>
         <p className="project-description">{description}</p>
         <a href={url} target="_blank" rel="noopener noreferrer">
-          <button className="project-button">Zur Website</button>
+          <button className="project-button">Visit Website</button>
         </a>
       </div>
 
       {/* Right Section - Image */}
       <div className="project-image fly-in-from-right image-drop-shadow">
         <div className="cover">
-          <img
-            className="laptop-frame"
-            src={laptopImg}
-            alt="Laptop frame"
-          />
+          <img className="laptop-frame" src={laptopImg} alt="Laptop frame" />
           <img
             className="screenshot"
             src={image}
             alt={`Screenshot of ${title}`}
           />
+          {isHovered && (
+            <a
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="visit-website-btn"
+            >
+              Visit Website
+            </a>
+          )}
         </div>
       </div>
     </div>
