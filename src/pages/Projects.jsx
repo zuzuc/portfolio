fd543e7a2c081ceb84371e3a8c003f8a9458ecae
zuzuc <zuzu@posteo.de>
2025-01-31 18:21:48 +0100

    Set container div for portfolio page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,30 +8,32 @@ function Projects() {
   return (
     <>
       <Navbar />
-      <div className="web-dev-title-container">
-        <h1 className="web-dev-title">Web Development</h1>
-      </div>
-      <div className="headline-container">
-        <div className="section-title">
-          <h2>Portfolio</h2>
+      <div className="portfolio-page">
+        <div className="web-dev-title-container">
+          <h1 className="web-dev-title">Web Development</h1>
         </div>
-        <article className="portfolio-intro">
-          A selection of projects I´ve been working on so far, as part of the
-          privately, as part of the Coding Bootcamp or at work{" "}
-        </article>
-      </div>
+        <div className="headline-container">
+          <div className="section-title">
+            <h2>Portfolio</h2>
+          </div>
+          <article className="portfolio-intro">
+            A selection of projects I´ve been working on so far, as part of the
+            privately, as part of the Coding Bootcamp or at work{" "}
+          </article>
+        </div>
 
-      <div className="projects-container">
-        {projectData.map((project) => (
-          <ProjectCard
-            key={project.title}
-            title={project.title}
-            tags={project.techStacks}
-            description={project.description}
-            image={project.image}
-            url={project.url}
-          />
-        ))}
+        <div className="projects-container">
+          {projectData.map((project) => (
+            <ProjectCard
+              key={project.title}
+              title={project.title}
+              tags={project.techStacks}
+              description={project.description}
+              image={project.image}
+              url={project.url}
+            />
+          ))}
+        </div>
       </div>
       <Footer />
     </>
