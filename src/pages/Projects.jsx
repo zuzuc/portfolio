376b70263ea97d8b7b092a6d86a19130bd43d160
zuzuc <zuzu@posteo.de>
2025-01-31 20:54:53 +0100

    Add content to portfolio page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,30 +9,46 @@ function Projects() {
     <>
       <Navbar />
       <div className="portfolio-page">
-        <div className="web-dev-title-container">
-          <h1 className="web-dev-title">Web Development</h1>
-        </div>
-        <div className="headline-container">
-          <div className="section-title">
-            <h2>Portfolio</h2>
+        <div className="portfolio-container">
+          <div className="web-dev-title-container">
+            <h1 className="web-dev-title">Web Development</h1>
           </div>
-          <article className="portfolio-intro">
-            A selection of projects I´ve been working on so far, as part of the
-            privately, as part of the Coding Bootcamp or at work{" "}
+          <article className="web-dev-intro">
+            I am an analytical and creative web developer with an
+            interdisciplinary background and over three years of experience in
+            building design systems and pattern libraries using native web
+            technologies. <br />
+            <br />
+            Passionate about component-based development, I have gained hands-on
+            experience with React and am actively expanding my expertise in web
+            accessibility. Committed to creating inclusive digital experiences,
+            I have deepened my knowledge through extensive study and completion
+            of the W3Cx: Introduction to Web Accessibility course, enabling me
+            to develop user-friendly and compliant products.
           </article>
-        </div>
+          <div className="headline-container">
+            <div className="section-title">
+              <h2>Portfolio</h2>
+            </div>
+            <article className="portfolio-intro">
+              A selection of projects I´ve been working on so far, privately or
+              as part of the training to become a "Full Stack Web & App
+              Developer" at WBS Coding School.{" "}
+            </article>
+          </div>
 
-        <div className="projects-container">
-          {projectData.map((project) => (
-            <ProjectCard
-              key={project.title}
-              title={project.title}
-              tags={project.techStacks}
-              description={project.description}
-              image={project.image}
-              url={project.url}
-            />
-          ))}
+          <div className="projects-container">
+            {projectData.map((project) => (
+              <ProjectCard
+                key={project.title}
+                title={project.title}
+                tags={project.techStacks}
+                description={project.description}
+                image={project.image}
+                url={project.url}
+              />
+            ))}
+          </div>
         </div>
       </div>
       <Footer />
